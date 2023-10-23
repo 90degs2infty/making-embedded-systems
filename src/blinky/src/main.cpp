8859a607edc24cbfb9305ea829cafc84254396d2
90degs2infty <90degs2infty@posteo.org>
2023-10-23 10:52:42 +0200

    Implement blinky application

--- src/blinky/src/main.cpp
+++ src/blinky/src/main.cpp
@@ -4,11 +4,45 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <stdio.h>
+#include <Display.h>
+
+#include <zephyr/kernel.h>
+
+#include <cstdio>
 
 int main(void)
 {
-	printf("This is using cpp\n");
 	printf("Hello World! %s\n", CONFIG_BOARD);
+
+	auto disp = Display::init();
+	if (!disp.has_value())
+	{
+		printf("Failed to initialize display.\n");
+		printf("Entering busy wait loop.\n");
+
+		while (true)
+		{
+		}
+	}
+	else
+	{
+		bool on = false;
+		while (true)
+		{
+			if (on)
+			{
+				disp->turnBlack();
+			}
+			else
+			{
+				disp->turnWhite();
+			}
+			on = !on;
+			k_msleep(1000);
+		}
+
+		disp->turnWhite();
+	}
+
 	return 0;
 }
