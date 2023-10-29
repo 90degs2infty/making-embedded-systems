e4f77ebe6a0b3e5a89f88728b135de23735c727f
90degs2infty <90degs2infty@posteo.org>
2023-10-29 12:25:31 +0100

    Read from serial blockingly

--- homework/week_5/command-rs/src/bin/minimal.rs
+++ homework/week_5/command-rs/src/bin/minimal.rs
@@ -16,6 +16,9 @@ mod app {
         },
         pac::UARTE0,
     };
+    use rtic_monotonics::nrf::rtc::{ExtU64, Rtc0};
+
+    const COMMAND_BUFFER_SIZE: usize = 8;
 
     // Shared resources go here
     #[shared]
@@ -44,12 +47,10 @@ mod app {
             Baudrate::BAUD115200,
         );
 
-        // TODO setup monotonic if used
-        // let sysclk = { /* clock setup + returning sysclk as an u32 */ };
-        // let token = rtic_monotonics::create_systick_token!();
-        // rtic_monotonics::systick::Systick::new(cx.core.SYST, sysclk, token);
+        let token = rtic_monotonics::create_nrf_rtc0_monotonic_token!();
+        Rtc0::start(board.RTC0, token);
 
-        task1::spawn().ok();
+        command_client::spawn().ok();
 
         (
             Shared {
@@ -69,17 +70,28 @@ mod app {
         }
     }
 
-    // TODO: Add tasks
     #[task(priority = 1, local = [ serial ])]
-    async fn task1(cx: task1::Context) {
-        defmt::info!("Hello from task1!");
+    async fn command_client(cx: command_client::Context) {
+        defmt::trace!("Starting client of command pattern.");
 
-        // Force msg to be located in RAM
-        let mut msg = [0u8; 16];
-        msg.copy_from_slice(b"Hello via UART\r\n");
-
+        let mut buf = [0u8; COMMAND_BUFFER_SIZE];
         let serial = cx.local.serial;
 
-        let _ = serial.write(&msg).unwrap();
+        loop {
+            // Read COMMAND_BUFFER_SIZE bytes from serial blockingly
+            let bytes = match serial.read(&mut buf) {
+                // let bytes = match serial.read_timeout(&mut buf, timer, 1_000) {
+                Ok(()) => buf.len(),
+                _ => 0,
+            };
+
+            if bytes > 0 {
+                defmt::trace!("Got '{}'", buf[..bytes]);
+                let _ = serial.write(&buf[..bytes]).unwrap();
+            }
+
+            // Let the scheduler switch to a different task before reading again
+            Rtc0::delay(5.millis()).await
+        }
     }
 }
