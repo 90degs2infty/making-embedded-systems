2ef920feee72a5b31e9b4b8b0ebb7b9de44a46aa
90degs2infty <90degs2infty@posteo.org>
2023-11-25 20:14:55 +0100

    Dump linker defined variables

--- homework/week_8/src/bin/minimal.rs
+++ homework/week_8/src/bin/minimal.rs
@@ -24,16 +24,106 @@ mod app {
         // TODO: Add resources
     }
 
+    macro_rules! get_dunder {
+        ($dst:ident, $src:ident) => {
+            let $dst: usize;
+            unsafe {
+                $dst = &$src as *const u8 as usize;
+            }
+        };
+    }
+
     #[init]
     fn init(_cx: init::Context) -> (Shared, Local) {
         defmt::info!("init");
 
-        // TODO setup monotonic if used
-        // let sysclk = { /* clock setup + returning sysclk as an u32 */ };
-        // let token = rtic_monotonics::create_systick_token!();
-        // rtic_monotonics::systick::Systick::new(cx.core.SYST, sysclk, token);
+        extern "C" {
+            static mut __pre_init: u8;
+            static mut _stack_start: u8;
 
-        task1::spawn().ok();
+            static mut __vector_table: u8;
+            static mut __reset_vector: u8;
+            static mut __eexceptions: u8;
+
+            static mut __stext: u8;
+            static mut __etext: u8;
+
+            static mut __srodata: u8;
+            static mut __erodata: u8;
+
+            static mut __sdata: u8;
+            static mut __edata: u8;
+
+            static mut __sidata: u8;
+            static mut __veneer_base: u8;
+            static mut __veneer_limit: u8;
+
+            static mut __sbss: u8;
+            static mut __ebss: u8;
+
+            static mut __suninit: u8;
+            static mut __euninit: u8;
+
+            static mut __sheap: u8;
+        }
+
+        // get all the __foo values from above
+        get_dunder!(pre_init, __pre_init);
+        get_dunder!(stack_start, _stack_start);
+
+        get_dunder!(vector_table, __vector_table);
+        get_dunder!(reset_vector, __reset_vector);
+        get_dunder!(eexceptions, __eexceptions);
+
+        get_dunder!(stext, __stext);
+        get_dunder!(etext, __etext);
+
+        get_dunder!(srodata, __srodata);
+        get_dunder!(erodata, __erodata);
+
+        get_dunder!(sdata, __sdata);
+        get_dunder!(edata, __edata);
+
+        get_dunder!(sidata, __sidata);
+        get_dunder!(veneer_base, __veneer_base);
+        get_dunder!(veneer_limit, __veneer_limit);
+
+        get_dunder!(sbss, __sbss);
+        get_dunder!(ebss, __ebss);
+
+        get_dunder!(suninit, __suninit);
+        get_dunder!(euninit, __euninit);
+
+        get_dunder!(sheap, __sheap);
+
+        // dump all the __foo values via RTT
+        defmt::info!("pre_init '0x{:x}'", pre_init);
+        defmt::info!("stack_start '0x{:x}'", stack_start);
+
+        defmt::info!("vector_table '0x{:x}'", vector_table);
+        defmt::info!("reset_vector '0x{:x}'", reset_vector);
+        defmt::info!("eexceptions '0x{:x}'", eexceptions);
+
+        defmt::info!("stext '0x{:x}'", stext);
+        defmt::info!("etext '0x{:x}'", etext);
+
+        defmt::info!("srodata '0x{:x}'", srodata);
+        defmt::info!("erodata '0x{:x}'", erodata);
+
+        defmt::info!("sdata '0x{:x}'", sdata);
+        defmt::info!("edata '0x{:x}'", edata);
+
+        defmt::info!("sidata '0x{:x}'", sidata);
+        defmt::info!("veneer_base '0x{:x}'", veneer_base);
+        defmt::info!("veneer_limit '0x{:x}'", veneer_limit);
+
+        defmt::info!("sbss '0x{:x}'", sbss);
+        defmt::info!("ebss '0x{:x}'", ebss);
+
+        defmt::info!("suninit '0x{:x}'", suninit);
+        defmt::info!("euninit '0x{:x}'", euninit);
+
+        defmt::info!("sheap '0x{:x}'", sheap);
 
         (
             Shared {
@@ -52,11 +142,5 @@ mod app {
 
         #[allow(clippy::empty_loop)]
         loop {}
-    }
-
-    // TODO: Add tasks
-    #[task(priority = 1)]
-    async fn task1(_cx: task1::Context) {
-        defmt::info!("Hello from task1!");
     }
 }
